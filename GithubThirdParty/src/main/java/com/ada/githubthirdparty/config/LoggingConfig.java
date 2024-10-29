6d172a04aaf5643977fbcd68a3293fe7b2ef8f92
m.shams <mahurshams94@gmail.com>
2024-10-29 10:52:55 +0330

    Added my logging lib

---
+++ GithubThirdParty/src/main/java/com/ada/githubthirdparty/config/LoggingConfig.java
@@ -0,0 +1,21 @@
+package com.ada.githubthirdparty.config;
+
+
+import com.ada.logginglib.filter.LoggingFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+public class LoggingConfig {
+
+    @Bean
+    public LoggingFilter loggingFilter(ObjectMapper objectMapper) {
+        LoggingFilter loggingFilter = new LoggingFilter(objectMapper);
+
+
+//        loggingFilter.setLogFormat("ISO27001");
+
+        return loggingFilter;
+    }
+}
--- GithubThirdParty/src/main/java/com/ada/githubthirdparty/filter/LoggingFilter.java
+++ GithubThirdParty/src/main/java/com/ada/githubthirdparty/filter/LoggingFilter.java
@@ -1,50 +1,50 @@
-package com.ada.githubthirdparty.filter;
-
-
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-
-
-@Component
-public class LoggingFilter implements Filter {
-
-    private final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
-
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
-        if (servletRequest instanceof HttpServletRequest && servletResponse instanceof HttpServletResponse) {
-            HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-            HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
-
-
-            long startTime = System.currentTimeMillis();
-
-            logger.info("Incoming request: {} {}?{}",
-                    httpServletRequest.getMethod(),
-                    httpServletRequest.getRequestURI(),
-                    httpServletRequest.getQueryString() != null ? httpServletRequest.getQueryString() : "");
-
-
-            filterChain.doFilter(servletRequest, servletResponse);
-
-
-
-            long duration = System.currentTimeMillis() - startTime;
-            logger.info("Outgoing response: status={} duration={}ms", httpServletResponse.getStatus(), duration);
-        }else {
-            filterChain.doFilter(servletRequest, servletResponse);
-        }
-
-    }
-
-
-}
+//package com.ada.githubthirdparty.filter;
+//
+//
+//import jakarta.servlet.*;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Component;
+//
+//import java.io.IOException;
+//
+//
+//@Component
+//public class LoggingFilter implements Filter {
+//
+//    private final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+//
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//
+//        if (servletRequest instanceof HttpServletRequest && servletResponse instanceof HttpServletResponse) {
+//            HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+//            HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+//
+//
+//            long startTime = System.currentTimeMillis();
+//
+//            logger.info("Incoming request: {} {}?{}",
+//                    httpServletRequest.getMethod(),
+//                    httpServletRequest.getRequestURI(),
+//                    httpServletRequest.getQueryString() != null ? httpServletRequest.getQueryString() : "");
+//
+//
+//            filterChain.doFilter(servletRequest, servletResponse);
+//
+//
+//
+//            long duration = System.currentTimeMillis() - startTime;
+//            logger.info("Outgoing response: status={} duration={}ms", httpServletResponse.getStatus(), duration);
+//        }else {
+//            filterChain.doFilter(servletRequest, servletResponse);
+//        }
+//
+//    }
+//
+//
+//}
