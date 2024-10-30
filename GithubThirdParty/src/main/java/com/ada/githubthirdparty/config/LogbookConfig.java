ede65fad7ff0b968fe30248adec0affa009de612
m.shams <mahurshams94@gmail.com>
2024-10-30 16:17:45 +0330

    Added logbook

--- GithubThirdParty/src/main/java/com/ada/githubthirdparty/config/LogbookConfig.java
+++ GithubThirdParty/src/main/java/com/ada/githubthirdparty/config/LogbookConfig.java
@@ -1,15 +1,19 @@
-//package com.ada.githubthirdparty.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.zalando.logbook.Logbook;
-//import org.zalando.logbook.core.Conditions;
-//import org.zalando.logbook.core.DefaultHttpLogFormatter;
-//import org.zalando.logbook.core.DefaultHttpLogWriter;
-//import org.zalando.logbook.core.DefaultSink;
-//
-//@Configuration
-//public class LogbookConfig {
+package com.ada.githubthirdparty.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.zalando.logbook.HttpRequest;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.core.Conditions;
+import org.zalando.logbook.core.DefaultHttpLogFormatter;
+import org.zalando.logbook.core.DefaultHttpLogWriter;
+import org.zalando.logbook.core.DefaultSink;
+import org.zalando.logbook.json.JsonHttpLogFormatter;
+
+import java.util.function.Predicate;
+
+@Configuration
+public class LogbookConfig {
 //    @Bean
 //    public Logbook logbook() {
 //        Logbook logbook = Logbook.builder()
@@ -20,6 +24,26 @@
 //                .build();
 //        return logbook;
 //    }
-//}
-//
-//
+
+
+    @Bean
+    public Logbook logbook() {
+        Predicate<HttpRequest> exclude = Conditions.exclude(
+                Conditions.requestTo("/repos").or(
+                        Conditions.requestTo("/userInfo"))
+        );
+
+        //DefaultSink httpSink = new DefaultSink(new DefaultHttpLogFormatter(), new DefaultHttpLogWriter());
+
+        //DefaultSink curlSink = new DefaultSink(new CurlHttpLogFormatter(), new DefaultHttpLogWriter());
+
+        DefaultSink jsonSink = new DefaultSink(new JsonHttpLogFormatter(), new DefaultHttpLogWriter());
+
+        return Logbook.builder()
+                .condition(exclude)
+                .sink(jsonSink)
+                .build();
+    }
+}
+
+
