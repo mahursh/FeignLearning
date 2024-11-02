d67490b49834f24d609b5543fbfd154c8707ed0d
m.shams <mahurshams94@gmail.com>
2024-11-02 15:26:07 +0330

    update

--- GithubThirdParty/src/main/java/com/ada/githubthirdparty/config/LogbookConfig.java
+++ GithubThirdParty/src/main/java/com/ada/githubthirdparty/config/LogbookConfig.java
@@ -1,19 +1,27 @@
 package com.ada.githubthirdparty.config;
 
+import com.ada.githubthirdparty.customes.CustomSink;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.zalando.logbook.HttpRequest;
 import org.zalando.logbook.Logbook;
-import org.zalando.logbook.core.Conditions;
-import org.zalando.logbook.core.DefaultHttpLogFormatter;
-import org.zalando.logbook.core.DefaultHttpLogWriter;
-import org.zalando.logbook.core.DefaultSink;
-import org.zalando.logbook.json.JsonHttpLogFormatter;
 
-import java.util.function.Predicate;
+
 
 @Configuration
 public class LogbookConfig {
+
+
+
+
+    @Bean
+    public Logbook logbook() {
+        return Logbook.builder()
+                .sink(new CustomSink())
+                .build();
+    }
+
+
+
 //    @Bean
 //    public Logbook logbook() {
 //        Logbook logbook = Logbook.builder()
@@ -25,25 +33,46 @@ public class LogbookConfig {
 //        return logbook;
 //    }
 
+//
+//    @Bean
+//    public Logbook logbook() {
+////        Predicate<HttpRequest> exclude = Conditions.exclude(
+////                Conditions.requestTo("/userInfo")
+////        );
+//
+//        //DefaultSink httpSink = new DefaultSink(new DefaultHttpLogFormatter(), new DefaultHttpLogWriter());
+//
+//        //DefaultSink curlSink = new DefaultSink(new CurlHttpLogFormatter(), new DefaultHttpLogWriter());
+//
+//        DefaultSink jsonSink = new DefaultSink(new JsonHttpLogFormatter(), new DefaultHttpLogWriter());
+//
+//        return Logbook.builder()
+//                .correlationId(new DefaultCorrelationId())
+////                .condition(exclude)
+//                .sink(jsonSink)
+//                .build();
+//    }
+////
+//    @Bean
+//    public Logbook logbook() {
+//        BinaryOperator<String> filterSecretHeader = (s1, s2)-> s1.toLowerCase().equals("x-secret") ? "<secret>" : s2;
+//
+//        return Logbook.builder()
+//                .queryFilter(accessToken())
+//                .queryFilter(replaceQuery("password", "<secret>"))
+//                .headerFilter(authorization())
+//                .headerFilter(eachHeader(filterSecretHeader))
+//                .bodyFilter(
+//                        BodyFilters.replaceJsonStringProperty(
+//                                new HashSet<>(Arrays.asList("password")), "<secret>"))
+//                .formatter(new JsonHttpLogFormatter())
+//                .writer(new DefaultHttpLogWriter(
+//                        LoggerFactory.getLogger(LogbookConfig.class),
+//                        DefaultHttpLogWriter.Level.INFO))
+//                .build();
+//    }
 
-    @Bean
-    public Logbook logbook() {
-        Predicate<HttpRequest> exclude = Conditions.exclude(
-                Conditions.requestTo("/repos").or(
-                        Conditions.requestTo("/userInfo"))
-        );
 
-        //DefaultSink httpSink = new DefaultSink(new DefaultHttpLogFormatter(), new DefaultHttpLogWriter());
-
-        //DefaultSink curlSink = new DefaultSink(new CurlHttpLogFormatter(), new DefaultHttpLogWriter());
-
-        DefaultSink jsonSink = new DefaultSink(new JsonHttpLogFormatter(), new DefaultHttpLogWriter());
-
-        return Logbook.builder()
-                .condition(exclude)
-                .sink(jsonSink)
-                .build();
-    }
 }
 
 
