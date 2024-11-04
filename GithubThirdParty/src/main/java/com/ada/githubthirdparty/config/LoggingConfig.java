7400544262076b92412ede0562b2eaabeda62d3a
m.shams <mahurshams94@gmail.com>
2024-11-04 16:37:52 +0330

    update

--- GithubThirdParty/src/main/java/com/ada/githubthirdparty/config/LoggingConfig.java
+++ GithubThirdParty/src/main/java/com/ada/githubthirdparty/config/LoggingConfig.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Map;
 import java.util.Set;
 
 
@@ -17,11 +18,16 @@ public class LoggingConfig {
     public LoggingFilter loggingFilter(ObjectMapper objectMapper) {
         LoggingFilter loggingFilter = new LoggingFilter(objectMapper);
 
-        loggingFilter.setMaskedParameters(Set.of("(?i)password", "(?i)creditCard"));
         loggingFilter.setIgnoredPaths(Set.of("/health", "/info", "/userInfo/.*"));
+
         loggingFilter.setExcludedBodyPaths(Set.of("/login", "/register"));
 
         loggingFilter.setLogFormat("JSON");
+
+        loggingFilter.setMaskingRules(Map.of(
+                "(?i)url", "*****",
+                "(?i)creditCard", "####-####-####"
+        ));
 
         return loggingFilter;
     }
