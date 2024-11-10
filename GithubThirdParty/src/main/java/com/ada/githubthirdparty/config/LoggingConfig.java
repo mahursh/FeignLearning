cab3980b357c3899d3b85663f545684833028eb3
m.shams <mahurshams94@gmail.com>
2024-11-10 16:08:48 +0330

    update

--- GithubThirdParty/src/main/java/com/ada/githubthirdparty/config/LoggingConfig.java
+++ GithubThirdParty/src/main/java/com/ada/githubthirdparty/config/LoggingConfig.java
@@ -1,36 +1,37 @@
-//package com.ada.githubthirdparty.config;
-//
-//
-//import com.ada.logginglib.filter.LoggingFilter;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Bean;
-//
-//import java.util.Map;
-//import java.util.Set;
-//
-//
-//@Configuration
-//public class LoggingConfig {
-//
-//
-//    @Bean
-//    public LoggingFilter loggingFilter(ObjectMapper objectMapper) {
-//        LoggingFilter loggingFilter = new LoggingFilter(objectMapper);
-//
-//        loggingFilter.setIgnoredPaths(Set.of("/userInfo/.*"));
-//
-////        loggingFilter.setExcludedBodyPaths(Set.of("/login", "/register"));
-//
-//        loggingFilter.setLogFormat("JSON");
-//
-//        loggingFilter.setMaskingRules(Map.of(
-//                "METHOD", "*****",
-//                "creditCard", "####",
-//                "(?i)UrL", "#####",
-//                "STATUS\\d+", "****"
-//        ));
-//
-//        return loggingFilter;
-//    }
-//}
+package com.ada.githubthirdparty.config;
+
+
+import com.ada.logginglib.filter.LoggingFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Map;
+import java.util.Set;
+
+
+@Configuration
+public class LoggingConfig {
+
+
+    @Bean
+    public LoggingFilter loggingFilter(ObjectMapper objectMapper) {
+        LoggingFilter loggingFilter = new LoggingFilter(objectMapper);
+
+        loggingFilter.setIgnoredPaths(Set.of("/userInfo/.*"));
+
+//        loggingFilter.setExcludedBodyPaths(Set.of("/login", "/register"));
+
+        loggingFilter.setLogFormat("JSON");
+
+        loggingFilter.setMaskingRules(Map.of(
+                "METHOD", "*****",
+                "creditCard", "####",
+                "(?i)UrL", "#####",
+                "STATUS\\d+", "****"
+
+        ));
+
+        return loggingFilter;
+    }
+}
